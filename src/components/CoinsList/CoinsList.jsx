313eca715d42f51b5b95746f02e69019e71da440
varavoi <institut93@gmail.com>
2025-10-25 16:43:44 +0300

    стилизовал CoinsList

--- src/components/CoinsList/CoinsList.jsx
+++ src/components/CoinsList/CoinsList.jsx
@@ -4,8 +4,21 @@ const CoinsList = ({coins}) => {
     return (
         <ul className='coins-list'>
            {coins.map(coin=>(
-                <li key={coin}>
-                        {coin.symbol+'/'+coin.name}
+                <li className='coin-item' key={coin}>
+                        <div className='coin-item__info'>
+                            <img className='coin-item__logo' src={coin.iconUrl} alt={coin.name} />
+                            <p style={{color:coin.color}}>
+                                {coin.name}/USD
+                            </p>
+                        </div>
+                        <div className='coin-item__price'>
+                            <p style={{color:coin.color}}>
+                                {(+coin.price).toFixed(2)} USD
+                            </p>
+                             <p style={{color:coin.color}}>
+                                {(+coin.btcPrice).toFixed(2)} BTC
+                            </p>
+                        </div>
                 </li>
            ))}
         </ul>

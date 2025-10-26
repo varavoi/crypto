c364a0a51816fdec2bbe2f5da694bd11f6cbdf36
varavoi <institut93@gmail.com>
2025-10-26 19:20:11 +0300

    мемоизация coins дороже 1000usd

--- src/components/CoinsList/CoinsList.jsx
+++ src/components/CoinsList/CoinsList.jsx
@@ -4,7 +4,7 @@ const CoinsList = ({coins}) => {
     return (
         <ul className='coins-list'>
            {coins.map(coin=>(
-                <li className='coin-item' key={coin}>
+                <li className='coin-item' key={coin.uuid}>
                         <div className='coin-item__info'>
                             <img className='coin-item__logo' src={coin.iconUrl} alt={coin.name} />
                             <p style={{color:coin.color}}>
